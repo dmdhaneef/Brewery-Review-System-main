c482d1f5a73b6e697a0363ce855edd4a9d09e3ae
dmdhaneef <dmdhaneef786@gmail.com>
2024-06-23 14:57:54 +0530

    first commit

---
+++ Brewery-Review-System-main/README.md
@@ -0,0 +1,29 @@
+# Brewery-Review-System
+Brewery review system is fully fledged website designed using -HTML -CSS -Bootstrap Framework -JavaScript -PHP -MYSQL Database
+
+This is a dynamic website which uses both front-end and back-end tools. Front-end tools are HTML, CSS, Bootstrap Framework, JavaScript. Back-end programming is done using PHP which is used to link front-end with back-end database. MYSQL is the database which stores details and user's credential.
+
+How to run?
+
+Step 1: Download the code from Github repository. Extract the zip folder.
+
+Step 2: Download XAMPP Control Panel. By default a folder named XAMPP will be stored in C Drive.
+
+Step 3: In XAMPP folder find htdocs folder. Copy and paste the extracted zip folder into htdocs holder.
+
+Step 4: Open XAMPP Control Panel. Start the Apache and MYSQL modules. After starting the MYSQL module click on Admin. This takes us to phpMyAdmin.
+
+Step 5: Click on new and create a new Database. In our case database name is "reviews_db".
+
+Step 6: Once Database is created successfully, click on import. Import the database which resides in SQL folder of the downloaded source code.
+
+step 7: Now type localhost/brewery(folder name) in your favourite web browser. A localhost(127.0.0.1) for your system will be created which runs the PHP code efficiently with fully functional database.
+
+![image](https://github.com/SANJEEV61/Brewery-Review-System/assets/89179742/dfb28454-5acd-4e12-9fcc-c0e4c9e39041)
+![Screenshot (101)](https://github.com/SANJEEV61/Brewery-Review-System/assets/89179742/0f23884a-cf75-4e9e-9747-5de827595ce7)
+![Screenshot (103)](https://github.com/SANJEEV61/Brewery-Review-System/assets/89179742/67d9ca9f-9eb7-4842-94e1-9944163867b3)
+![Screenshot (104)](https://github.com/SANJEEV61/Brewery-Review-System/assets/89179742/39079580-94a2-4a45-bd6c-255250ccb6f2)
+![Screenshot (105)](https://github.com/SANJEEV61/Brewery-Review-System/assets/89179742/acc0d5f5-477d-47de-becb-892fd0357a6b)
+![Screenshot (106)](https://github.com/SANJEEV61/Brewery-Review-System/assets/89179742/4463a8a3-19c6-480c-9b3b-6cbb54d500f2)
+
+
---
+++ Brewery-Review-System-main/reviews_db.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 19, 2023 at 05:46 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `reviews_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` varchar(20) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `image` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `image`) VALUES
+('h12FAnxY6JoXb51iDpda', 'Karl Strauss Brewing Co - 4S Ranch', 'post_1.webp'),
+('sRKX0vSREJbBzO07wM1H', 'Sea Dog Brewing Co - S. Portland', 'post_2.webp'),
+('G6zDaxTTS0fV5UT4BQ46', 'US Beer Brewers At the Cellar Door', 'post_3.webp'),
+('6zQRsklaYIO38cLIgYZN', 'Stubborn German Brewing', 'post_4.webp'),
+('mMj2FWPRVWZPsfOsjSUL', 'WarPigs USA Brewing Co.', 'post_5.webp'),
+('hK2tgabAaK1c1FAak6UW', 'Ursa Minor Brewing', 'post_6.webp');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reviews`
+--
+
+CREATE TABLE `reviews` (
+  `id` varchar(20) NOT NULL,
+  `post_id` varchar(20) NOT NULL,
+  `user_id` varchar(20) NOT NULL,
+  `rating` varchar(1) NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `description` varchar(1000) NOT NULL,
+  `date` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `image` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
